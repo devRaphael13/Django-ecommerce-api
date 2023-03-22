04772c536706b2b5f2427d6d2aa52c396d9ed3a5
Raphael Ezeigwe <iraphael1308@gmail.com>
2023-03-22 16:01:35 +0100

    modify admin

--- api/admin.py
+++ api/admin.py
@@ -1,10 +1,37 @@
 from django.contrib import admin
-from api.models import Category, Product, Brand, Cart, Order, Image, User
+from api.models import (
+    User,
+    Cart,
+    Order,
+    OrderItem,
+    Image,
+    SizeChart,
+    Size,
+    Variant,
+    Product,
+    Brand,
+    Category,
+    Message,
+    Account,
+    Bank,
+    Transfer,
+    Review,
+)
 
-admin.site.register(Category)
-admin.site.register(Image)
-admin.site.register(Product)
-admin.site.register(Brand)
+admin.site.register(User)
 admin.site.register(Cart)
 admin.site.register(Order)
-admin.site.register(User)
+admin.site.register(OrderItem)
+admin.site.register(Image)
+admin.site.register(SizeChart)
+admin.site.register(Size)
+admin.site.register(Variant)
+admin.site.register(Product)
+admin.site.register(Brand)
+admin.site.register(Category)
+admin.site.register(Message)
+admin.site.register(Account)
+admin.site.register(Bank)
+admin.site.register(Transfer)
+admin.site.register(Review)
+
