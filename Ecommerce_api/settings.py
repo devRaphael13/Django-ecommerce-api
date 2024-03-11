52e3e71b74d25d5d0f3068770f3d747594f5a5c3
Raphael Ezeigwe <iraphael1308@gmail.com>
2024-03-11 11:46:22 +0100

    Chore: clean up redundant settings

--- Ecommerce_api/settings.py
+++ Ecommerce_api/settings.py
@@ -140,8 +140,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles", "static")
 
 AUTH_USER_MODEL = 'api.User'
 
@@ -149,15 +147,6 @@ AUTH_USER_MODEL = 'api.User'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = os.getenv('EMAIL_HOST')
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-PAYSTACK_SECRET_KEY = os.getenv('PAYSTACK_SECRET_KEY')
 
 CACHES = {
     "default": {
--- Ecommerce_api/urls.py
+++ Ecommerce_api/urls.py
@@ -23,7 +23,3 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('api.urls'))
 ]
-
-urlpatterns += [
-    path('api-auth', include('rest_framework.urls'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
