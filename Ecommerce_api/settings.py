b56e233cdc844eb3b5110642634bfc0d0980ae5f
Raphael Ezeigwe <iraphael1308@gmail.com>
2024-04-06 13:34:35 +0100

    Feat: Only JSON representation

--- Ecommerce_api/settings.py
+++ Ecommerce_api/settings.py
@@ -83,6 +83,12 @@ TEMPLATES = [
 WSGI_APPLICATION = "Ecommerce_api.wsgi.application"
 
 REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
+    'DEFAULT_PARSER_CLASSES': (
+        'rest_framework.parsers.JSONParser',
+    ),
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.TokenAuthentication"
     ],
