72a31a3a1194460f969616e62354739399295ece
Raphael <raphael.ezeigwe@yahoo.com>
2024-07-13 19:08:20 +0100

    Add static root

--- Ecommerce_api/settings.py
+++ Ecommerce_api/settings.py
@@ -148,7 +148,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "static_root")
 
 AUTH_USER_MODEL = "api.User"
 
