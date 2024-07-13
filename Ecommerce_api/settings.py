5f7bbc16a270f14274da9041ca6df5b6305bb592
Raphael <raphael.ezeigwe@yahoo.com>
2024-07-13 20:42:57 +0100

    Fix staticfiles

--- Ecommerce_api/settings.py
+++ Ecommerce_api/settings.py
@@ -147,8 +147,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static_root")
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 AUTH_USER_MODEL = "api.User"
 
--- build_files.sh
+++ build_files.sh
@@ -1,1 +1,2 @@
-pip install -r requirements.txt
+python3 -m pip install -r requirements.txt
+python3 manage.py collectstatic --noinput
